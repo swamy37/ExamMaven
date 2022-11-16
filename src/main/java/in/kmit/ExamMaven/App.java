2234fe3f9eb5c67e4fe7fa5b02efba919ac8f9f4
Narasimha Swamy <thalishettynarasimhaswamy37@gmail.com>
2022-11-16 12:17:18 +0530

    send

---
+++ src/main/java/in/kmit/ExamMaven/App.java
@@ -0,0 +1,13 @@
+package in.kmit.ExamMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE lab internal 1 exam, The created project is ExamMaven " );
+    }
+}
